d646b4e31a2865e97656915d18129d7ffc376689
Pinkypre <preciouspncube@gmail.com>
2024-02-09 21:18:11 +0000

    tasks

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,46 +1,72 @@
 #!/usr/bin/python3
+"""
+Module 0-nqueens
+A program that solves the N queens problem
+"""
 import sys
 
-def is_safe(board, row, col, N):
-    # Check if there is a queen in the same column
-    for i in range(row):
-        if board[i] == col or board[i] - i == col - row or board[i] + i == col + row:
-            return False
-    return True
+if len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
 
-def solve_nqueens(board, row, N, solutions):
-    if row == N:
-        solutions.append([[i, board[i]] for i in range(N)])
-        return
+try:
+    num = int(sys.argv[1])
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
 
-    for col in range(N):
-        if is_safe(board, row, col, N):
-            board[row] = col
-            solve_nqueens(board, row + 1, N, solutions)
+if not (num >= 4):
+    print("N must be at least 4")
+    sys.exit(1)
 
-def print_solutions(solutions):
-    for solution in solutions:
-        print(solution)
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
+def solveNQueens(n):
+    """Solution for n queens"""
+    col = set()  # keep track of used columns
+    pos = set()  # (r + c) keep track of used positive diagonals
+    neg = set()  # (r - c) keep track of used negative diagonals
 
-    try:
-        N = int(sys.argv[1])
-        if N < 4:
-            print("N must be at least 4")
-            sys.exit(1)
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
+    res = []  # final result
 
-    board = [-1] * N
-    solutions = []
+    board = [[] for n in range(n)]  # create empy board
 
-    solve_nqueens(board, 0, N, solutions)
-    print_solutions(solutions)
+    def backtrack(row):
+        """function for recursion"""
+        # means we've reached the last row
+        if row == n:
+            # get copy of current solution(current board)
+            copy = board.copy()
+            res.append(copy)
+            return
+
+        # for every column
+        for c in range(n):
+            # if we find that the column or diagonals are used, then skip
+            if c in col or (row + c) in pos or (row - c) in neg:
+                continue
+
+            # register found columns and diagonals
+            col.add(c)
+            pos.add(row + c)
+            neg.add(row - c)
+
+            board[row] = [row, c]
+
+            # move to next row
+            backtrack(row + 1)
+
+            # finally undo
+            col.remove(c)
+            pos.remove(row + c)
+            neg.remove(row - c)
+            board[row] = []
+
+    backtrack(0)
+
+    return res
+
 
 if __name__ == "__main__":
-    main()
+    boards = solveNQueens(num)
+    for board in boards:
+        print(board)
